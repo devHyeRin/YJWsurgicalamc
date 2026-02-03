a71054a58d189b93efae06e055f648a45fe4d064
devHyeRin <hyerinlee0v0@gmail.com>
2026-02-04 01:53:42 +0900

    푸터: 카피라이트 추가

---
+++ client/src/components/layout/Footer/FooterCopyright.tsx
@@ -0,0 +1,11 @@
+import "./Footer.css";
+
+function FooterCopyright(){
+
+    return(
+
+        <p>© {new Date().getFullYear()} HBM Labs. Designed & Developed by HBM Labs.</p>
+
+    );
+}
+export default FooterCopyright;
