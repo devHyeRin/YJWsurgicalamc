5be6590346dd9fc1c0a9612d14a147d48a3ec8a1
devHyeRin <hyerinlee0v0@gmail.com>
2026-02-05 20:58:51 +0900

    푸터: 전체 구조/css 확정

--- client/src/components/layout/Footer/Footer.tsx
+++ client/src/components/layout/Footer/Footer.tsx
@@ -7,10 +7,15 @@ function Footer(){
 
     return(
         <footer className={`site-footer ${styles.footer}`}>
-            <div className={`container ${styles.inner}`}>
-                <FooterInfo/>
-                <FooterLinks/>
-                <FooterCopyright/>
+            <div className="container">
+                {/* 상단 영역 */}
+                <div className={styles.inner}>
+                    <FooterInfo />
+                    <FooterLinks />
+                </div>
+
+                {/* 하단 카피라이트 */}
+                <FooterCopyright />
             </div>
         </footer>
     );
